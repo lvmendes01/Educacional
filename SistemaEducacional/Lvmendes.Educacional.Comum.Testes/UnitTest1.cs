e6790caae3f619b868364152bd1a79aefd707fe3
Leonardo Mendes <leonardoviniciusmendes@hotmail.com>
2023-07-19 11:51:05 -0300

    testes

---
+++ SistemaEducacional/Lvmendes.Educacional.Comum.Testes/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Lvmendes.Educacional.Comum.Testes
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
