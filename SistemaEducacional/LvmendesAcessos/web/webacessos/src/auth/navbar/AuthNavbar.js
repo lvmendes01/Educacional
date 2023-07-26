83180cec20a50df3eacf05a32ef2a17788f2cf39
Leonardo Mendes <leonardoviniciusmendes@hotmail.com>
2023-07-26 12:40:59 -0300

    ajustes

---
+++ SistemaEducacional/LvmendesAcessos/web/webacessos/src/auth/navbar/AuthNavbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+const AuthNavbar = () => {
+    return (
+        <React.Fragment>
+            <Navbar bg="dark" expand="lg" className="navbar-dark">
+                <Container>
+                    <Navbar.Brand>React Auth Demo</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </React.Fragment>
+    );
+}
+export default AuthNavbar;
